7faea59b3ffc529c561bf30392e2f112b1449435
root <root@junislee>
2024-05-05 14:34:16 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use rcli01::{Opts, SubCommand, process_csv};
+
+
+fn main() -> anyhow::Result<()> {
+    let opts: Opts = Opts::parse();
+    match opts.cmd {
+        SubCommand::Csv(opts) => 
+            process_csv(&opts.input, &opts.output)?,
+    }
+    Ok(())
+
 }
